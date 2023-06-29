69b0d4510ab24d7717ddd9981d535abcbbd6196b
italomaia03 <italo.fernandes.om@gmail.com>
2023-06-29 13:04:55 -0300

    activate mongoose getters when passing to JSON

--- src/models/Tutor.ts
+++ src/models/Tutor.ts
@@ -2,19 +2,28 @@ import { Schema, model } from "mongoose";
 import { ITutor } from ".";
 import { randomUUID } from "crypto";
 
-const tutorSchema = new Schema<ITutor>({
-    _id: {
-        type: Schema.Types.UUID,
-        default: () => randomUUID(),
+const tutorSchema = new Schema<ITutor>(
+    {
+        _id: {
+            type: Schema.Types.UUID,
+            default: () => randomUUID(),
+        },
+        name: { type: String, required: true },
+        password: {
+            type: String,
+            required: true,
+            get: () => {
+                return "*******";
+            },
+        },
+        phone: { type: String, required: true },
+        email: { type: String, required: true },
+        date_of_birth: { type: Date, required: true },
+        zip_code: { type: String, required: true },
+        pets: [Schema.Types.UUID],
     },
-    name: { type: String, required: true },
-    password: { type: String, required: true },
-    phone: { type: String, required: true },
-    email: { type: String, required: true },
-    date_of_birth: { type: Date, required: true },
-    zip_code: { type: String, required: true },
-    pets: [Schema.Types.UUID],
-});
+    { versionKey: false, timestamps: false, toJSON: { getters: true } }
+);
 
 const Tutor = model<ITutor>("Tutors", tutorSchema);
 export default Tutor;
